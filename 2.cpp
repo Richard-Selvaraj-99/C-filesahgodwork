d22df94feb65c298f7d9a2008782c7d17f644e4d
richard selvaraj <r10053506@gmail.com>
2026-04-14 14:08:17 +0530

    cin cout

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short age;
+    string name;
+
+    cout<<"enter name";
+    cin>>name;
+
+    cout<<"enter age";
+    cin>>age;
+
+    cout<<"wow your name is :"<<name<<endl;
+    cout<<"you age is "<<age<<endl;
+    return 0;
+}
