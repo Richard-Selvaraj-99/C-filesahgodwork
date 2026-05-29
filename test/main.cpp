0a862c17273178b9166dab80178f4d9d6f03c675
richard selvaraj <r10053506@gmail.com>
2026-05-29 07:53:00 +0530

    c++ relearning

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age = 26;
+    string name = "google";
+    bool booolluu = true;
+    double experience = 0.8;
+    signed int onlypos = 2;
+    unsigned int onlyneg = -2;
+    auto calibrate = "this lets the compiler understand what variable this is";
+    cout<<"mon age"<<age<<"is it true "<<booolluu<<endl;
+    cout<<"mon years du experince est mal ?"<<experience<<endl;
+    cout<<"mon age"<<onlypos<<"onlyneg"<<onlyneg<<endl;
+    return 0;
+
+}
